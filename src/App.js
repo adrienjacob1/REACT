24f2641bbc6deb9d30c6cb25cbab6445b3b0baca
adrien <adrienjacob1111@gmail.com>
2023-08-02 14:27:38 +0200

    commit props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import MyInfo from "./components/MyInfo"
 
 // class App() {}
 
 
 /* Quand le composant est rendu par une fonction on l'appelle Composant Fonctionnel*/
 function App() {
+
+  const dataToPass = "Irlande";
+
   return (
     <div className="App"> { /* JS dans la paire d'accolades et JSx a l'extérieur des accolades*/ }
     {/* pour écrire un commentaire dans la partie JSX du fichier, il faut l'enturer d'accolades */}
@@ -27,6 +31,7 @@ function App() {
       <section></section>
       <h2>Exemple</h2>
       <p></p>
+      <MyInfo propertyReceivingData={dataToPass} />
     </div>
     //<div></div>
   );
---
+++ src/components/MyInfo.js
@@ -0,0 +1,55 @@
+import "./MyInfo.css"
+
+function MyInfo({ propertyReceivingData }  ) {
+
+    const isEmpty = false;
+
+    const secondList = true;
+
+    const liElement = <li>USA</li>;   // ligne de code JS car pas dans le return
+
+    const countryName = "Thailande";
+
+    const tableauElements = [1, 2, 3];
+    const elementsMap = tableauElements.map(i => <p> { i } </p>);
+
+    const counter = 3;
+
+    const styles = {
+        color: "#F00",
+        backgroundColor: "#0F0",
+        fontSize: "1.2rem"   // si je veux des pixels en unité, je peux simplement écrire le nombre de pixel en type nombre
+    }
+
+    if (isEmpty) {
+        return <p>Il n'y a pas de données à afficher</p>;
+    }
+
+    return (
+        <div className="someClass">
+                <h2>Adrien</h2>
+                <p style={styles}>Ma liste de destination de voyage :</p>
+                { !secondList && <ul>
+                    <li>Nouvelle Zelande</li>
+                    <li>Australie</li>
+                    <li>Chili</li>
+                    { liElement /* ligne JS (entre accolades)*/}           
+                </ul>  }
+
+                {secondList && <ul>
+                    <li>Chypre</li>
+                    <li> { propertyReceivingData } </li>
+                    <li> { `J'aimerai aussi aller en ${countryName}` } </li>
+                    { liElement /* ligne JS (entre accolades)*/}           
+                </ul>
+                }
+                <p>nombre de destinations: {counter + 1}</p>
+
+                { elementsMap }
+            
+        
+        </div>
+    );
+}
+
+export default MyInfo;
